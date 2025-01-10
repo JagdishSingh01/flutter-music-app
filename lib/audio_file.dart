1463d9a3046c41e72ee3ffb245ea2f5726259364
JagdishSingh01 <jatinpawar354@gmail.com>
2025-01-10 18:57:35 +0530

    added audioplayer package.

---
+++ lib/audio_file.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AudioFile extends StatefulWidget {
+  const AudioFile({super.key});
+
+  @override
+  State<AudioFile> createState() => _AudioFileState();
+}
+
+class _AudioFileState extends State<AudioFile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/detail_audio_page.dart
+++ lib/detail_audio_page.dart
@@ -13,6 +13,7 @@ class _DetailAudioPageState extends State<DetailAudioPage> {
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
+      backgroundColor: AppColors.audioBluishBackground,
       body: Stack(
         children: [
           Positioned(
@@ -30,16 +31,67 @@ class _DetailAudioPageState extends State<DetailAudioPage> {
               right: 0,
               child: AppBar(
                 leading: IconButton(
-                icon:Icon(Icons.arrow_back_ios),
+                icon:Icon(Icons.arrow_back_ios,color: Colors.white,),
                 onPressed : (){},
                 ),
                 actions:[
                   IconButton(
-                    icon: Icon(Icons.search),
+                    icon: Icon(Icons.search,color: Colors.white),
                     onPressed: (){},
                   )
-                ]
+                ],
+                backgroundColor: Colors.transparent,
               )
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: screenHeight*0.2,
+            height: screenHeight*0.36,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color:Colors.white,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: screenHeight*0.1,),
+                  Text(
+                    "THE WATER CURE",style:TextStyle(fontSize: 30, fontWeight: FontWeight.bold, fontFamily: 'Avenir')
+                  ),
+                  Text(
+                    "Martin Hyatt", style: TextStyle(fontSize: 20),
+                  ),
+                  
+                  
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top:screenHeight*0.12,
+            left: (screenWidth-150)/2,
+            right: (screenWidth-150)/2,
+            height: screenHeight*0.16,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(17),
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                    image: DecorationImage(
+                      image: AssetImage("img/skel.jpg"),
+                      fit: BoxFit.cover
+                    ),
+                  ),
+                ),
+              ),
+            )
           ),
         ],
       ),
